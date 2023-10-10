e1a22d1aa32b1cc063652f418b33fcade8e0575d
VVladds <72264245+VVladds@users.noreply.github.com>
2023-10-10 17:26:10 +0400

    1.2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ window = display.set_mode((700,500))
 display.set_caption('Shooter')
 
 background = transform.scale(image.load('354282-admin.jpg'), (700,500))
-
+background.set_alpha(45)
 
 '''mixer.init()
 mixer.music.load('space.ogg')
@@ -16,8 +16,7 @@ font.init()
 
 game = True
 finish = False
-lost = 0
-kills = 0
+
 class GameSprite(sprite.Sprite):
     def __init__(self,imag,x,y,speed):
         self.image = transform.scale(image.load(imag),(25,100))
@@ -71,38 +70,41 @@ class Bullet(GameSprite):
 hero = Player('fffs.png', 20, 10, 10,0)
 hero2 = Player('fffs.png', 650, 10, 10,1)
 
-ball = Bullet('ball.png',100,100,1)
+ball = Bullet('ball.png',100,100,5)
 Players = list()
 Players.append(hero)
 Players.append(hero2)
 
-score = font.Font(None,30).render('Счет: ' + str(kills), True,(255,255,255))
-miss = font.Font(None,30).render('Пропущено: ' + str(lost), True,(255,255,255))
+
 win = font.Font(None,70).render('YOU WIN!', True,(255,215,0))
 lose = font.Font(None,70).render('YOU LOSE!', True,(255,215,0))
 
 while game:
+    while finish != True:
 
-    for e in event.get():
-            if e.type == QUIT:
-                game = False
-    clock = time.Clock()
-    window.blit(background,(0,0))
-    ball.reset()
-    ball.update()
+        for e in event.get():
+                if e.type == QUIT:
+                    game = False
+        clock = time.Clock()
+        window.blit(background,(0,0))
+        ball.reset()
+        ball.update()
 
-    for hero in Players:
-        hero.update()
-        hero.reset()
-        if sprite.collide_rect(ball, hero):
-            ball.speed_x*=-1
+        for hero in Players:
+            hero.update()
+            hero.reset()
+            if sprite.collide_rect(ball, hero):
+                ball.speed_x*=-1
+
+        if ball.rect.x <=0:
+            finish = True
+            window.blit(lose,(0,0))
 
 
 
 
-
-    clock.tick(FPS)
-    display.update()
+        clock.tick(FPS)
+        display.update()
 
 
 
